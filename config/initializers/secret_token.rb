044a2a5c07064db7d3ce0d03ec761f72ecf65efa
Raphael Molesim <raphael.sm86@gmail.com>
2012-09-15 03:44:45 +0000

    Primeiro Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quemeuvoto::Application.config.secret_token = '46a2adfe4b04f5233acf5e94eac0fd15c2c01b152ac33526d3b0625fbc6f741daae2f50e480a61fc25df578b4ec62f097d615a983d86378b0e0f6b3f35e3db19'
