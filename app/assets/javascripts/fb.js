1e403128509a822ae765dfc6fdb57520ee403ac8
Raphael Molesim <raphael.sm86@gmail.com>
2012-09-16 18:30:56 -0300

    facebook integrado

--- app/assets/javascripts/fb.js
+++ app/assets/javascripts/fb.js
@@ -2,18 +2,18 @@ FB.init({appId: "241454712643631", status: true, cookie: true});
 
 $("a.facebook").live ('click', function(e) {
   e.preventDefault();
-  
+
   var obj = {
     method: 'feed',
     link: 'http://www.emquemeuvoto.com.br/',
-    picture: 'http://fbrell.com/f8.jpg',
-    name: 'Facebook Dialogs',
-    caption: 'Reference Documentation',
-    description: 'Using Dialogs to interact with users.'
+    picture: 'http://www.emquemeuvoto.com.br/assets/logo.jpg',
+    name: 'Em Quem Eu Voto?',
+    caption: 'Podemos te ajudar a escolher um candidato para esta próxima eleição municipal!',
+    description: 'Você já decidiu em quem vai votar nesta eleição municipal? Ainda não? Que tal conhecer um pouco mais sobre as propóstas dos candidados? Nós podemos recomendar um cadidato baseado nas propóstas que você concorda ou discorda! Venha conhecer um pouco mais!'
   };
 
   function callback(response) {
-    document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
+    //document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
   }
 
   FB.ui(obj, callback);
