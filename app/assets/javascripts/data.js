4a54cf1c89b5af59ca017dda49636f116e71bb39
Raphael Molesim <raphael.sm86@gmail.com>
2012-09-17 11:48:21 -0300

    Mudei o data.js

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -2,71 +2,71 @@ var questions = [
 
 	{
 		title: 'Pedágio Urbano',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'soninha' ],
 		opponents: [ 'serra', 'haddad', 'russomano' ]
 	},
 	{
 		title: 'Escola Período Integral',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'haddad' ],
 		opponents: [ ]
 	},
 	{
-		title: 'Tarifa Porpocional',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		title: 'Tarifa do Ônibus Propocional',
+		subtitle: '',
+		description: '',
 		proposers: [ 'russomano' ],
 		opponents: [ ]
 	},
 	{
-		title: 'Extensão da Marginal até Guaianazes e da Radial Leste',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		title: 'Extensão da Marginal até Guaianazes e extensão da Radial Leste',
+		subtitle: '',
+		description: '',
 		proposers: [ 'serra' ],
 		opponents: [ ]
 	},
 	{
 		title: 'Repovoar o Centro',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'soninha', 'haddad', 'russomano' ],
 		opponents: [ ]
 	},
 	{
 		title: 'Inclusão Social de Travestis, Gay, Lésbicas e Bisexuais',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'soninha', 'haddad' ],
 		opponents: [ ]
 	},
 	{
 		title: 'Investimento da Prefeitura em Metrô, trens ou Monotrilho',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'serra', 'russomano' ],
 		opponents: [ 'soninha' ]
 	},
 	{
 		title: 'Urbanização de Favelas',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'serra', 'russomano', 'haddad' ],
 		opponents: [ ]
 	},
 	{
 		title: 'Descentralização da Gestão Pública',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'soninha', 'haddad' ],
 		opponents: [ 'serra' ]
 	},
 	{
 		title: 'Aumentar o número de professores na sala de aula',
-		subtitle: 'Subtítulo a ser feito',
-		description: 'Descrição a ser feito',
+		subtitle: '',
+		description: '',
 		proposers: [ 'soninha', 'serra' ],
 		opponents: [  ]
 	}
