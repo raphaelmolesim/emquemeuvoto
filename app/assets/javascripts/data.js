5d3aac47dc0cd1715d006ac22181057dd7fa349d
Raphael Molesim <raphael.sm86@gmail.com>
2012-09-17 15:47:17 -0300

    Adicionei algumas descricoes

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -5,7 +5,7 @@ var questions = [
 		subtitle: 'Desestimular o uso do automóvel em zonas congestionadas e bem servidas de transporte público',
 		description: 'Realizadas algumas obras de melhoria no sistema de transporte e na integração entre automóvel-transporte coletivo (com a ampliação do número de garagens junto a estações de trem, metrô e ônibus fora da região central), será estabelecido um perímetro dentro do qual a circulação em automóvel particular, nos horários de maior movimento, estará sujeita à cobrança de uma taxa. Toda a arrecadação será investida na melhoria do transporte coletivo.<br /><br />A arrecadação será monitorada em tempo real por um sistema informatizado que  poderá ser acompanhado pela internet, garantindo sua total transparência, o que é  fundamental para o sucesso do sistema. Serão definidos os projetos no sistema de transporte público que receberão essas receitas de maneira que o benefício será visível e fiscalizável por qualquer cidadão.',
 		proposers: [ 'soninha' ],
-		opponents: [ 'serra', 'haddad', 'russomano' ]
+		opponents: [ 'serra', 'haddad', 'russomano', 'chalita' ]
 	},
 	{
 		title: 'Escola Período Integral',
@@ -23,29 +23,33 @@ var questions = [
 	},
 	{
 		title: 'Tarifa do Ônibus Proporcional',
-		subtitle: '',
-		description: '',
+		subtitle: 'O valor da passagem varia conforme a distância percorrida',
+		description: 'Instituir a tarifa proporcional, estabelecendo valor mínimo e máximo ao usuário do sistema de transporte de ônibus, onde será pago apenas o percurso utilizado.<br /><br /> Assim quem anda distâncias maiores paga mais e que anda distâncias menores paga menos.',
 		proposers: [ 'russomano' ],
-		opponents: [ ]
+		opponents: [ ],
+		references: [ 
+			{ 'Proposta do Russomano' : 'http://celsorussomanno.com.br/propostas/instituir-a-tarifa-proporcional-estabelecendo-valor-minimo-e-maximo-ao-usuario-do-sistema-de-transporte-de-onibus-onde-sera-pago-apenas-o-percurso-utilizado/' }
+		]
 	},
 	{
 		title: 'Extensão da Marginal até Guaianazes e extensão da Radial Leste',
-		subtitle: '',
-		description: '',
+		subtitle: 'Desenvolvimento da Zona Leste começando por obras viárias',
+		description: 'Nossa proposta é desenvolver a criação de empregos regionais em São Paulo. Aqui vamos fazer um pólo industrial e tecnológico. Para isso, tem quer te obras viárias. <br /><br />Vamos prolongar a marginal até Guaianazes e (vamos prolongar) a Radial Leste, que vem lá de Arthur Alvim. Até Guaianazes, são cerca de 9 ou 10 km descontínuos. (Também) vamos fazer corredor de ônibus na Radial Leste inteira. Enfim, temos muito a fazer, muito já foi feito e muito será feito no futuro',
 		proposers: [ 'serra' ],
-		opponents: [ ]
+		opponents: [ ],
+		references: [ { 'Promessa de José Serra' : 'http://noticias.terra.com.br/eleicoes/2012/sp/sao-paulo/noticias/0,,OI6100999-EI20654,00-Serra+vai+de+helicoptero+a+zona+leste+e+promete+melhorar+transporte.html' } ]
 	},
 	{
 		title: 'Repovoar o Centro',
-		subtitle: '',
-		description: '',
+		subtitle: 'Criar condições para a utilização de edifícios no centro da cidade, para uso de moradias sociais',
+		description: 'A administração municipal precisa garantir que pessoas de todas as classes sócioeconômicas possam morar na região central - especialmente as de renda mais baixa, às quais o mercado imobiliário não se dedica tanto. <br /><br /> A prefeitura pode investir diretamente na produção de moradia popular (com a construção de novas unidades e a reforma de imóveis desapropriados) e estabelecer várias formas de relacionamento com o mercado – restringindo, onerando, incentivando de modo a conduzir/ induzir o investimento privado na direção do interesse coletivo',
 		proposers: [ 'soninha', 'haddad', 'russomano', 'chalita' ],
 		opponents: [ ]
 	},
 	{
 		title: 'Inclusão Social de Travestis, Gay, Lésbicas e Bisexuais',
-		subtitle: '',
-		description: '',
+		subtitle: 'Desenvolver campanhas de convivência pacífica',
+		description: 'Combater defasagem de LGBTs da sala de aula e fomentar em programas de educação de jovens e adultos a inclusão social de LGBTs.<br /><br />Realizar formação continuada dos profissionais da rede municipal de ensino e desenvolver programas para aceitação de diferença junto aos estudantes e família.',
 		proposers: [ 'soninha', 'haddad' ],
 		opponents: [ ]
 	},
